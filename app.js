46ee290807fe661fe8563dfb67e8b4789d50bf43
Ubuntu <ubuntu@ip-172-31-87-149.ec2.internal>
2020-05-27 00:33:04 +0000

    yea

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var mailRoutes = require('./routes/mail.js');
 var app = express();
 
 console.log("buenos");
-
+app.get('/', (req, res) => {
+  res.send('HEY!')
+});
 
 app.use('/mail',mailRoutes);
 
 //Get the server running
 const port = process.env.PORT || 5000;
 const host = process.env.HOST || 'localhost';
-app.listen(port, host);
-console.log(`Your server is running on ${host}:${port}`);
+//app.listen(port, host);
+//console.log(`Your server is running on ${host}:${port}`);
+app.listen(5000, () => console.log('Server running on port 5000'))
